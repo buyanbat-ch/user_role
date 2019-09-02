19a7529a03c4e540b0698b96d9a12caa15364a04
Tsengel Purevdorj <tsnglp@gmail.com>
2019-09-02 16:12:40 +1000

    Add new, create method to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,27 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all    
+    @users = User.all
   end
 
   def new
-
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @saved = @user.save
+    if @saved
+      flash[:notice] = "Saved successfully"
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
+private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
 
 end
